8c04135c5d33cc1e885f11b46a29ccd08b463fe7
Antunes <gabrielantunescontato@gmail.com>
2024-04-09 19:03:23 -0300

    add terminal mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -9,3 +9,6 @@ map("i", "jj", "<ESC>")
 
 map("n", "<leader>gsj", "<cmd> GoTagAdd json <CR>", { desc = "Add json struct tags" })
 map("n", "<leader>gsy", "<cmd> GoTagAdd yaml <CR>", { desc = "Add yaml struct tags" })
+map({ "n", "t" }, "<leader>tf", function()
+  require("nvchad.term").toggle { pos = "float", id = "floatTerm" }
+end, { desc = "Terminal Toggle Floating term" })
