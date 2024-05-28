d9cc4e22d4ef49742408b6af97d3ee459bf08216
Antunes Gabriel <gabrielantunescontato@gmail.com>
2024-05-28 11:02:06 -0300

    add noice

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -109,4 +109,19 @@ return {
   {
     "windwp/nvim-ts-autotag",
   },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
 }
